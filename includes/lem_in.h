d30c1b7f9cb45081104a23b666172a4fe3086182
Vincent Duong <vduong@imac-de-vincent-1.home>
2018-09-28 14:26:40 +0200

    header updated

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -8,4 +8,25 @@ typedef struct	s_lemin
 	int			nbant;
 }				t_lemin;
 
+typedef struct s_record
+{
+	char	*ops;
+	int		nbturns;
+}
+
+typedef struct	s_path
+{
+		int		*cases;
+		int		length;
+}								t_op;
+
+typedef struct	s_map
+{
+	int			nbants;
+	int			nbstart;
+	int			nbend;
+	int			**graph;
+	t_path	*paths;
+}								t_map;
+
 #endif
