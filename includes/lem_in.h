865114c28d81e36b28fd9aae009532fe5ac98786
Vincent Duong <vduong@imac-de-vincent-1.home>
2018-09-28 14:32:12 +0200

    header updated

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -8,11 +8,19 @@ typedef struct	s_lemin
 	int			nbant;
 }				t_lemin;
 
-typedef struct s_record
+typedef struct	s_ants
+{
+	int nbants;
+	int	nbstart;
+	int nbend;
+	int **rep;
+}								t_ants;
+
+typedef struct	s_record
 {
 	char	*ops;
 	int		nbturns;
-}
+}								t_record;
 
 typedef struct	s_path
 {
@@ -22,11 +30,10 @@ typedef struct	s_path
 
 typedef struct	s_map
 {
-	int			nbants;
-	int			nbstart;
-	int			nbend;
+	char		*cases;
 	int			**graph;
 	t_path	*paths;
+	s_ants	ants;
 }								t_map;
 
 #endif
