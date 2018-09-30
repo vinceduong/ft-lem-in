4d59c8750d56919beb0e5c369dd1ce957134ff8d
Vincent DUONG <vduong@e2r7p7.42.fr>
2018-09-30 14:44:33 +0200

    return 0 added

--- srcs/main.c
+++ srcs/main.c
@@ -7,26 +7,31 @@ int	main(void)
 	{
 		printf("Parsing failed");
 		printf("ERROR");
+		return (0);
 	}
 	if (!path(lemin))
 	{
 		printf("Pathing failed");
 		printf("ERROR");
+		return (0);
 	}
 	if (!split_ants(lemin))
 	{
 		printf("Splitting failed");
 		printf("ERROR");
+		return (0);
 	}
 	if (!record(lemin))
 	{
 		printf("Recording failed");
 		printf("ERROR");
+		return (0);
 	}
 	if (!display(lemin))
 	{
 		printf("Display failed");
 		printf("ERROR");
+		return (0);
 	}
 	return (0);
 }
