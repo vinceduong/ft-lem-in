fb97063e07c0d84a98cc6926f21c8b362b170513
Constantin ARMENIADES <carmenia@e3r3p10.42.fr>
2018-10-01 14:27:01 +0200

    amelioration record

--- srcs/record.c
+++ srcs/record.c
@@ -1,10 +1,10 @@
 #include "lem_in.h"
 
-char	*step_writer(t_lemin *lemin, int ant, int room);
+char	*step_writer(t_lemin *lemin, int antnum, int room);
 {
 	char *tmp;
 
-	tmp = ft_strjoin("L", ft_itoa(ant));
+	tmp = ft_strjoin("L", ft_itoa(antnum));
 	tmp = ft_strjoin(tmp, "-");
 	tmp = ft_strjoin(tmp, lemin->m->case[room]);
 	return (tmp);
@@ -12,25 +12,24 @@ char	*step_writer(t_lemin *lemin, int ant, int room);
 
 int	launch_path(t_lemin *lemin, int pathnumber)
 {
-	int ant;
+	int antpos;
 	int wave;
 	int	room;
 
-	ant = 1;
-	wave = 0;
+	antpos = 1;
 	while (lemin->a->rep[pathnumber][ant])
 	{
-		while (wave - ant + 1 < lemin->p[pathnumber]->length)
+		wave = antpos - 1;
+		while (wave - antpos + 1 < lemin->p[pathnumber]->length)
 		{
 			room = lemin->p->paths[pathnumber][wave - ant + 1];
-			ops[wave] = ft_strjoin(ops[wave], step_writer(lemin, ant, room));
+			ops[wave] = ft_strjoin(ops[wave],
+				step_writer(lemin, lemin->a->rep[pathnumber][antpos], room));
 			wave++;
 		}
 		a++;
 	}
 }
-i
-tintin
 
 int	record(t_lemin *lemin)
 {
