86d462a2ff8e51a81ef7a2309c6da6bfcc2bcfdd
Theo ESCRIVA <thescriv@student.42.fr>
2018-10-01 16:40:10 +0200

    V.1.1 SPLIT_ANTS

--- srcs/split_ants.c
+++ srcs/split_ants.c
@@ -1,68 +1,58 @@
 #include "lem_in.h"
 
-int 		**ft_length_of_each_path(t_lemin *lemin, int **lengthpath)
+int **ft_length_of_each_path(int **lpath, t_lemin *lemin)
 {
-	t_nodelist *i;
-	t_node *f;
-	int ipath;
+	int i;
 
-	f = i->start;
-	ipath = 0;
-	lengthpath = (int**)malloc(sizeof(int*) * lemin->p->nbpath);
-	while (f->next != NULL)
+	i = 0;
+	lpath = (int**)malloc(sizeof(int*) * lemin->nbpaths);
+	while (lemin.p[0].start->next != NULL)
 	{
-		lengthpath[ipath] = (int*)malloc(sizeof(int*) * f->nb);
-		f = f->next;
-		ipath++;
-	}
-	return (lengthpath);
-}
-
-static void ft_multipath(t_lemin *lemin, int **lengthpath, int path, int ant)
-{
-	t_nodelist *f;
-	t_node *i;
-	int iant;
-
-	f = i->start;
-	iant = 0;
-	while (f->next != NULL)
-	{
-		lemin->a->rep[path] = (int*)malloc(sizeof(int) * f->length + 1);
-		f = f->next;
-		path++;
-	}
-	path = 0;
-	while (ant < lemin->a->nbants)
-	{
-		lemin->a->rep[path++][iant] = ant;
-		if (path == lemin->p->nbpath)
-		{
-			iant++;
-			path = 0;
-		}
-		ant++;
+		lpath = (int*)malloc(sizeof(int) * 1);
+		lpath[i][0] = lemin.p[0].start.node->length;
+		lemin
 	}
 }
 
 int split_ants(t_lemin *lemin)
 {
-	int **lengthpath;
-	int ant;
+	int **lpath;
 
-	lengthpath = 0;
-	ant = 0;
-	lemin->a->rep = (int**)malloc(sizeof(int*) * lemin->p->nbpath);
-	if (lemin->p->nbpath == 1)
-	{
-		lemin->a->rep[0] = (int*)malloc(sizeof(int) * lemin->a->nbants + 1);
-		while (ant < lemin->a->nbants)
-			lemin->a->rep[0][ant] = ant++;
-		lemin->a->rep[0][ant] = 0;
-	}
-	else
-	{
-		lengthpath = ft_length_of_each_path(lemin, lengthpath);
-		ft_multipath(lemin, lengthpath, 0, ant);
-	}
+	lpath = 0;
+	lpath = ft_length_of_each_path(lpath, lemin);
 }
+
+//lemin.p[0].start.node->length
+
+
+/*98 - 95 - 79 - 40
+
+7 - 4 = 3;
+15 - 7 = 8;
+38 - 15 = 13;
+ x / n = xf;
+
+
+while (fourmi)
+{
+	if (n == nbpaths)
+		reste = fourmi / chemin;
+	else if (reste > 0)
+	{
+		while (reste)
+		{
+			n = 0;
+			n == nbpaths ? n == 0 : 0;
+
+		}
+	}
+	ft_addition_path();
+}
+}
+
+
+4 = 3 + 8 + 13 + 10 == 	34
+7 = 8 + 13 + 10 == 		31
+15 = 13 + 10 == 		23
+38 = 10 = 				10
+*/
