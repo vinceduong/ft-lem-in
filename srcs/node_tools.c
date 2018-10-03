27588ec6a0e429198e1791e7dd3eee13cbf5c682
Vincent DUONG <vduong@e3r4p10.42.fr>
2018-10-03 11:26:23 +0200

    last_node added to path

--- srcs/node_tools.c
+++ srcs/node_tools.c
@@ -76,7 +76,10 @@ void					update_bl(t_nodelist *bl, t_path *path)
 	while (tmp)
 	{
 		if (!check_nodelist(bl, tmp->nb))
+		{
 			add_node(bl, new_node(tmp->nb));
+			return ;
+		}
 		tmp = tmp->next;
 	}
 }
