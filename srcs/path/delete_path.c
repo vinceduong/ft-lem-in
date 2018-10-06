18163ba6083eb1ca0d777b734c81617475f75a67
Vincent DUONG <vinceduong@hotmail.fr>
2018-10-06 15:25:38 +0200

    NO SEGFAULT ON DELETE_PATH HAHHAHAAHHAHAHHA

--- srcs/path/delete_path.c
+++ srcs/path/delete_path.c
@@ -8,14 +8,18 @@ t_path *delete_path(t_pathlist *paths, t_path *del)
 	tmp = paths->start;
 	if (tmp == del)
 	{
+		ft_putstr("In tmp == del");
 		tmp->previous = NULL;
 		return (paths->start = tmp->next);
 	}
 	while (tmp->next && tmp->next != del)
 		tmp = tmp->next;
 	tmp->next = tmp->next->next;
-	tmp->next->next->previous = tmp;
 	if (tmp->next)
+	{
 		tmp->next->previous = tmp;
+		if (tmp->next->next)
+			tmp->next->next->previous = tmp;
+	}
 	return (tmp->next);
 }
