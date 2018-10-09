cce8d5c6ad15d327ca7cacc9890bd24d7b363f9d
Vincent DUONG <vinceduong@hotmail.fr>
2018-10-09 19:01:05 +0200

    merge

--- srcs/path/count_new_childs.c
+++ srcs/path/count_new_childs.c
@@ -7,18 +7,9 @@ int	count_new_childs(t_lemin *l, t_path *p)
 
 	i = 0;
 	count = 0;
-<<<<<<< HEAD
-	print_matrix(l);
-	printf("Current node = %d\n", p->curr);
-	while (i < l->m.nbcases)
-	{
-		if (l->m.graph[p->curr][i] && l->m.graph[i][i] != 1 && l->m.graph[i][i] != -1)
-		{
-=======
 	while (i < l->m.nbcases)
 	{
 		if (l->m.graph[p->curr][i] && !l->m.graph[i][i])
->>>>>>> path
 			count++;
 		i++;
 	}
