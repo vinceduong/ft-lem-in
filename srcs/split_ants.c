5b26755d3f359d747a8e1c29466b3e9854f22e3b
Theo ESCRIVA <thescriv@student.42.fr>
2018-10-09 19:17:19 +0200

    dfa

--- srcs/split_ants.c
+++ srcs/split_ants.c
@@ -1,6 +1,6 @@
 #include "lem_in.h"
 
-static int ft_clean(int **lpath, int i)
+static int		ft_clean(int **lpath, int i)
 {
 	while (i)
 		free(lpath[i--]);
@@ -9,18 +9,17 @@ static int ft_clean(int **lpath, int i)
 	return (1);
 }
 
-static int **ft_assign_tube(t_lemin *lemin, int i, int nbant)
+static int		**ft_assign_tube(t_lemin *lemin, int i, int nbant)
 {
 	int j;
 
 	j = -1;
-	printf("nb ant = %d\n", nbant);
 	while (j++ != i)
 		lemin->a.rep[j][1] += nbant;
 	return (lemin->a.rep);
 }
 
-static int ft_how_many(t_lemin *lemin, int ant, int length, int i)
+static int		ft_how_many(t_lemin *lemin, int ant, int length, int i)
 {
 	int value;
 
@@ -36,7 +35,7 @@ static int ft_how_many(t_lemin *lemin, int ant, int length, int i)
 	return (value);
 }
 
-int ft_get_lost(t_lemin *lemin)
+int				ft_get_lost(t_lemin *lemin)
 {
 	int i;
 	int value;
@@ -51,7 +50,7 @@ int ft_get_lost(t_lemin *lemin)
 	return (value == lemin->a.nbants ? 0 : lemin->a.nbants - value);
 }
 
-int **ft_last_assign(t_lemin *lemin, int i, int nbant)
+int				**ft_last_assign(t_lemin *lemin, int i, int nbant)
 {
 	int j;
 
@@ -65,7 +64,22 @@ int **ft_last_assign(t_lemin *lemin, int i, int nbant)
 	return (lemin->a.rep);
 }
 
-static void ft_assign_ant(t_lemin *lemin)
+static int		ft_last_ant(t_lemin *lemin, int nbant, int ant, int i)
+{
+	while (ant)
+	{
+		nbant = ft_how_many(lemin, ant, lemin->a.rep[i][0], i);
+		if (nbant == 0)
+			break ;
+		lemin->a.rep = ft_assign_tube(lemin, i, nbant);
+		ant -= (nbant * (i + 1));
+	}
+	nbant = ft_get_lost(lemin);
+	lemin->a.rep = ft_last_assign(lemin, i, nbant);
+	return (0);
+}
+
+static void		ft_assign_ant(t_lemin *lemin)
 {
 	int i;
 	int ant;
@@ -74,24 +88,10 @@ static void ft_assign_ant(t_lemin *lemin)
 	i = 0;
 	nbant = 0;
 	ant = lemin->a.nbants;
-	printf("HOW MANY PATH %d\n", lemin->nbpaths);
-	while (ant)
+	while (ant > 0)
 	{
 		if (i == lemin->nbpaths - 1)
-		{
-			while (ant)
-			{
-				nbant = ft_how_many(lemin, ant, lemin->a.rep[i][0], i);
-				if (nbant == 0)
-					break ;
-				lemin->a.rep = ft_assign_tube(lemin, i, nbant);
-				ant -= (nbant * (i + 1));
-			}
-			nbant = ft_get_lost(lemin);
-			lemin->a.rep = ft_last_assign(lemin, i, nbant);
-			ant = 0;
-			break ;
-		}
+			ant = ft_last_ant(lemin, nbant, ant, i);
 		else
 		{
 			nbant = ft_how_many(lemin, ant, lemin->a.rep[i][0], i);
@@ -102,7 +102,7 @@ static void ft_assign_ant(t_lemin *lemin)
 	}
 }
 
-static void ft_get_length(t_lemin *lemin)
+static void		ft_get_length(t_lemin *lemin)
 {
 	int n;
 	int i;
@@ -121,29 +121,7 @@ static void ft_get_length(t_lemin *lemin)
 	}
 }
 
-/*void print_path_ant(t_lemin *lemin)
-{
-	int i;
-	int n;
-	int j;
-
-	j = 1;
-	i = 0;
-	n = 0;
-	while (n < lemin->nbpaths)
-	{
-		while (lemin->a.rep[n][j] != 0)
-		{
-			printf("%d   ", lemin->a.rep[n][j]);
-			j++;
-		}
-		printf("\n");
-		j = 1;
-		n++;
-	}
-}*/
-
-void ft_attribute_ant(t_lemin *lemin)
+static void		ft_attribute_ant(t_lemin *lemin)
 {
 	int n;
 	int i;
@@ -156,7 +134,8 @@ void ft_attribute_ant(t_lemin *lemin)
 		l = lemin->a.rep[i][1];
 		j = lemin->a.rep[i][0];
 		free(lemin->a.rep[i]);
-		lemin->a.rep[i] = (int*)malloc(sizeof(int) * l + 2);
+		if (!(lemin->a.rep[i] = (int*)malloc(sizeof(int) * l + 2)))
+			exit (0);
 		lemin->a.rep[i][l + 2] = 0;
 		lemin->a.rep[i][0] = l;
 		i++;
@@ -177,20 +156,10 @@ void ft_attribute_ant(t_lemin *lemin)
 	}
 }
 
-int split_ants(t_lemin *lemin)
+int				split_ants(t_lemin *lemin)
 {
 	ft_get_length(lemin);
 	ft_assign_ant(lemin);
 	ft_attribute_ant(lemin);
-	print_path_ant(lemin);
 	return (1);
 }
-
-/*98 - 95 - 79 - 40
- x / n = xf
-98
-4 = 3 + 8 + 13 + 10 == 	34
-7 = 8 + 13 + 10 == 		31
-15 = 13 + 10 == 		23
-38 = 10 = 				10
-*/
