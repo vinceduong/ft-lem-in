da99b816aa1caec6079ded721d4476b38af8d9c9
Constantin ARMENIADES <carmenia@e2r7p10.42.fr>
2018-10-15 15:54:21 +0200

    patch de leaks sur record et display

--- srcs/display.c
+++ srcs/display.c
@@ -1,5 +1,17 @@
 #include "lem_in.h"
 
+void free_ops(char **ops)
+{
+	int i;
+	
+	while (ops[i])
+	{
+		free(ops[i]);
+		i++;
+	}
+	free(ops);
+}
+
 int	display(t_lemin *lemin)
 {
 	int	i;
@@ -11,5 +23,6 @@ int	display(t_lemin *lemin)
 		ft_printf("%s \n", lemin->r.ops[i]);
 		i++;
 	}
+	free_ops(lemin->r.ops);
 	return (1);
 }
