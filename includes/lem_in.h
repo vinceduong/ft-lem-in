113ea0455a19c01705a21a2fe8a4242abe1c15d9
Vincent DUONG <vinceduong@hotmail.fr>
2018-10-15 10:31:21 +0200

    paths.h delete and lem_in.h normed

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -3,6 +3,7 @@
 
 # include "../libft/libft.h"
 # include <stdio.h>
+# include "paths.h"
 
 typedef struct		s_node
 {
@@ -67,39 +68,52 @@ typedef struct		s_lemin
 	int				nb_link;
 }					t_lemin;
 
-# include "paths.h"
-/* forme du graph :
-   0 1 2 3
-0[[0,0,0,0],
-1 [0,0,0,0],
-2 [0,0,0,0],
-3 [0,0,0,0]]*/
-
-void		print_matrix(t_lemin *l);
-void		print_ant_road(t_lemin *lemin);
-//On initialise map ants, si la map est invalide => return 0
-int			parser(t_lemin *lemin);
-//On cherche les plus courts chemins, si on trouve aucun path => return 0;
-int			paths(t_lemin *lemin);
-//On reparti les chemins pour les fourmis, on rempli ants->rep;
-int			split_ants(t_lemin *lemin);
-//On record les tours en faisant avancer les fourmis sur chaque path;
-int			record(t_lemin *lemin);
-//On affiche les tours sur l'entrée standard
-int			display(t_lemin *lemin);
-
-//foncitons de Cam
-void		ft_patatruc(char **tab, t_lemin *lemin, int *error_tab);
-void		ft_create_matrice(char **tab, t_lemin *lemin, int *error_tab);
-int			ft_chrstr(char *find, int c);
-int			ft_intlen(int nb);
-int			ft_coord_is_digit(char *str);
-//function spliting ant
-void		ft_get_length(t_lemin *lemin);
-int			**ft_realloc_ant(t_lemin *lemin, int i, int l, int j);
-int			ft_how_many(t_lemin *lemin, int ant, int i);
-int			**ft_assign_tube(t_lemin *lemin, int i, int nbant);
-int			**ft_last_assign(t_lemin *lemin, int i, int nbant);
-void		ft_clean(char **tab);
+void				print_matrix(t_lemin *l);
+void				print_ant_road(t_lemin *lemin);
+int					parser(t_lemin *lemin);
+int					paths(t_lemin *lemin);
+int					split_ants(t_lemin *lemin);
+int					record(t_lemin *lemin);
+int					display(t_lemin *lemin);
+void				ft_patatruc(char **tab, t_lemin *lemin, int *error_tab);
+void				ft_create_matrice(char **tab, t_lemin *l, int *t);
+int					ft_chrstr(char *find, int c);
+int					ft_intlen(int nb);
+int					ft_coord_is_digit(char *str);
+void				ft_get_length(t_lemin *lemin);
+int					**ft_realloc_ant(t_lemin *lemin, int i, int l, int j);
+int					ft_how_many(t_lemin *lemin, int ant, int i);
+int					**ft_assign_tube(t_lemin *lemin, int i, int nbant);
+int					**ft_last_assign(t_lemin *lemin, int i, int nbant);
+void				ft_clean(char **tab);
+t_node				*new_node(int nb);
+t_nodelist			*add_node(t_nodelist *nodelist, t_node *node);
+int					check_nodelist(t_nodelist *nodelist, int nodenb);
+t_path				*new_path(t_path *path, int nodenb);
+void				*add_path(t_pathlist *pathlist, t_path *path);
+t_path				*delete_path(t_pathlist *paths, t_path *del);
+t_nodelist			*cpy_nodelist(t_nodelist *src);
+t_path				*init_path(void);
+t_pathlist			*init_pathlist(void);
+t_nodelist			*init_nodelist(void);
+int					cpy_path(t_path *src, t_path *dest, t_node *node);
+t_pathlist			*findpathlist(t_lemin *l, t_pathlist *paths);
+void				add_new_paths(t_lemin *l, t_pathlist *new_paths, t_path *p);
+int					count_new_childs(t_lemin *l, t_path *p);
+int					fill_paths(t_lemin *l, t_pathlist *paths);
+t_pathlist			*new_path_list(t_lemin *l, t_pathlist *paths);
+void				sort_paths(t_pathlist *p, int (*c)(t_path *p1, t_path *p2));
+t_pathlist			*update_paths(t_lemin *l, t_pathlist *paths);
+int					paths(t_lemin *l);
+void				delete_used_paths(t_pathlist *p, t_path *f, int e);
+void				print_path(t_path *path);
+void				print_nodelist(t_nodelist *nl);
+void				print_path_list(t_pathlist *pl);
+void				map_dead_nodes(t_lemin *l);
+void				free_path(t_path *path);
+void				free_pathlist(t_pathlist *pathlist);
+void				free_nodelist(t_nodelist *nodelist);
+void				free_node(t_node *node);
+t_pathlist			*find_pathlist(t_lemin *l, t_pathlist *paths);
 
 #endif
