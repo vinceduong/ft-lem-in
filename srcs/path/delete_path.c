217a0ee74d9cf8e036347462f6580a4cc840f444
Vincent DUONG <vinceduong@hotmail.fr>
2018-10-15 13:47:12 +0200

    oupsi doupsi

--- srcs/path/delete_path.c
+++ srcs/path/delete_path.c
@@ -12,9 +12,10 @@ t_path	*delete_path(t_pathlist *paths, t_path *del)
 	}
 	while (tmp->next && tmp->next != del)
 		tmp = tmp->next;
+	tmp->next = tmp->next->next;
 	if (tmp->next)
 	{
-		tmp->next = tmp->next->next;
+		tmp->next->previous = tmp;
 		if (tmp->next->next)
 			tmp->next->next->previous = tmp;
 	}
