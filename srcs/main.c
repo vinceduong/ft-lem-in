239d8eb71adfbdf329786d240df77b83b622e4b2
Vincent DUONG <vinceduong@hotmail.fr>
2018-10-17 14:18:48 +0200

    cleaned

--- srcs/main.c
+++ srcs/main.c
@@ -9,19 +9,15 @@ int	main(void)
 		ft_printf("ERROR\n");
 		return (0);
 	}
-	ft_printf("Parsing worked\n");
 	if (!paths(&lemin))
 	{
 		ft_printf("ERROR\n");
 		return (0);
 	}
-	ft_printf("Pathing worked\n");
 	if (!split_ants(&lemin))
 		return (0);
-	ft_printf("Split ants worked\n");
 	if (!record(&lemin))
 		return (0);
-	ft_printf("Record worked\n");
 	if (!display(&lemin))
 		return (0);
 	return (0);
